74006bfd12c483e1371f726f6866e4930210ab4e
theFivehead <marekrohac5@fastmail.com>
2026-04-10 19:25:40 +0200

    change

--- wsl_start.ps1
+++
@@ -1,9 +0,0 @@
-$running = wsl --list --running
-
-if ($running -eq $null -or $running -eq "") {
-    Write-Output "WSL not running. Starting..."
-    wsl
-} else {
-    Write-Output "WSL already running:"
-    $running
-}
